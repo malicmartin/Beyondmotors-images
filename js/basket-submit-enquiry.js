4f2ed2020011549676626b1882e05aa314783a8d
Martin Malić <165675441+malicmartin@users.noreply.github.com>
2024-11-21 00:58:32 +0100

    Create basket-submit-enquiry.js

---
+++ js/basket-submit-enquiry.js
@@ -0,0 +1,1 @@
+function toggleEnquiryForm(){var form=document.getElementById('basket-enquiry-form');var button=document.getElementById('basket-submit-enquiry-btn');if(form.style.display==='none'){form.style.display='block';button.textContent='Hide Enquiry Form'}else{form.style.display='none';button.textContent='Open Enquiry Form'}}
